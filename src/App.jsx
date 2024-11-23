72af1a89f6914c20370df0f397ab3e504645cc86
Michael <mwu250@gmail.com>
2024-11-23 04:10:00 -0800

    reset to default

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import "./styles/Profile.css";
 
 function App() {
   return (
-    // <BrowserRouter basename="/BroncoHacks-Website/">
-      <BrowserRouter basename="/">
+    <BrowserRouter basename="/BroncoHacks-Website/">
+      {/* <BrowserRouter> */}
       <NavBar className="navbar" />
       <Routes>
         <Route path="/" element={<Home />} />
