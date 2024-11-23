d0e436bb0cac5c7173a054e7b1a46e77fbdca38e
Michael <mwu250@gmail.com>
2024-11-23 03:43:21 -0800

    testing custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom' 
-import './App.css'
-import NavBar from './Components/NavBar.jsx'
-import Home from './Pages/Home.jsx'
-import FAQ from './Pages/FAQ.jsx'
-import MeetTheTeam from './Pages/MeetTheTeam.jsx'
-import Schedule from './Pages/Schedule.jsx'
-import Sponsors from './Pages/Sponsors.jsx'
-import AboutUs from './Pages/About.jsx'
-import Footer from './Components/Footer.jsx'
-import Error from './Pages/Error.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar.jsx";
+import Home from "./Pages/Home.jsx";
+import FAQ from "./Pages/FAQ.jsx";
+import MeetTheTeam from "./Pages/MeetTheTeam.jsx";
+import Schedule from "./Pages/Schedule.jsx";
+import Sponsors from "./Pages/Sponsors.jsx";
+import AboutUs from "./Pages/About.jsx";
+import Footer from "./Components/Footer.jsx";
+import Error from "./Pages/Error.jsx";
 import "./styles/Profile.css"; /* temporary just so it visually looks fine */
 
 function App() {
   return (
-    <BrowserRouter basename='/BroncoHacks-Website/'>
+    // <BrowserRouter basename='/BroncoHacks-Website/'>
+    <BrowserRouter>
       <NavBar className="navbar" />
       <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/About" element={<AboutUs/>}/>
-          <Route path="/FAQ" element={<FAQ/>}/>
-          <Route path="/Team" element={<MeetTheTeam/>}/>
-          <Route path="/Sponsors" element={<Sponsors/>}/>
-          <Route path="/Schedule" element={<Schedule/>}/>
-          <Route path="/Error" element={<Error/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Team" element={<MeetTheTeam />} />
+        <Route path="/Sponsors" element={<Sponsors />} />
+        <Route path="/Schedule" element={<Schedule />} />
+        <Route path="/Error" element={<Error />} />
       </Routes>
-      <Footer/>
-
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 export default App;
