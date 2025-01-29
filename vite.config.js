86f2cc2ac00a372903e78b98e2d9b12baff03d83
DanielPasion <90020974+DanielPasion@users.noreply.github.com>
2025-01-29 13:56:45 -0800

    Vercel (#112)
    * vercel json
    
    * analytics

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
-})
+  plugins: [react(), vercel()],
+});
