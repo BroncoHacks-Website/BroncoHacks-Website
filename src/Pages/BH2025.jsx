9bd729a4559fcf515f253a96e298102dc3874b00
DanielPasion <dnpaxion@gmail.com>
2025-03-10 16:13:25 -0700

    Ch'

---
+++ src/Pages/BH2025.jsx
@@ -0,0 +1,33 @@
+import logo from "/src/Assets/Profile/icon/BroncoHacks_Logo.png";
+import "../styles/Schedule.css";
+import Accordion from "../Components/Accordion";
+import { accordionData } from "../Assets/Accordion/accordionData";
+
+function BH2025() {
+  return (
+    <div className="schedule-page">
+      <div className="schedule-title-container">
+        <h1 className="schedule-title">Schedule</h1>
+      </div>
+      <div className="schedule-container">
+        <div className="basic-info">
+          <img src={logo} alt="Logo" />
+          <div className="title">
+            <h1>BroncoHacks 2025</h1>
+            <div className="location-details">
+              <p>Date: April 18-19th 2025</p>
+              <p>Building: Building 163</p>
+              <p>Room: See Various Locations Below</p>
+            </div>
+          </div>
+        </div>
+
+        {accordionData.map((accordData, index) => (
+          <Accordion data={accordData} key={index}></Accordion>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BH2025;
