01f800838e6857bd12cddd1743f91635d4d73cb1
antonioj562 <antoniojloyola@gmail.com>
2026-02-20 00:36:09 -0800

    Skeleton setup for BroncoHacks 2026 - Moved Bhacks2025 code and assets into BroncoHacks2025 folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import NavBar from "./Components/NavBar.jsx";
 import Home from "./Pages/Home.jsx";
-import FAQ from "./Pages/FAQ.jsx";
-import MeetTheTeam from "./Pages/MeetTheTeam.jsx";
-import Schedule from "./Pages/Schedule.jsx";
-import Sponsors from "./Pages/Sponsors.jsx";
-import AboutUs from "./Pages/About.jsx";
-import BH2025 from "./Pages/BH2025.jsx";
-import Footer from "./Components/Footer.jsx";
-import Error from "./Pages/Error.jsx";
-import "./styles/Profile.css";
 
 function App() {
   return (
     // <BrowserRouter basename="/BroncoHacks-Website/">
     <BrowserRouter>
-      <NavBar className="navbar" />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<AboutUs />} />
-        <Route path="/FAQ" element={<FAQ />} />
-        <Route path="/Team" element={<MeetTheTeam />} />
-        <Route path="/Sponsors" element={<Sponsors />} />
-        <Route path="/Schedule" element={<Schedule />} />
-        <Route path="/BH2025" element={<BH2025 />} />
-        <Route path="/*" element={<Error />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/BroncoHacks2025/2025Pages/Home.jsx
@@ -0,0 +1,81 @@
+import Slideshow from "/src/Components/Slideshow.jsx";
+// import { slidesData } from "../Assets/Profile/slidesData";
+import DescriptionBox from "../Components/DescriptionBox";
+import Countdown from "../Components/Countdown";
+import "/src/styles/Home.css";
+import { useState } from "react";
+
+import Overview1 from "../Assets/BroncoHacks2025Photos/overview1.JPG";
+import Overview2 from "../Assets/BroncoHacks2025Photos/overview2.JPG";
+import Overview3 from "../Assets/BroncoHacks2025Photos/overview3.JPG";
+import Overview4 from "../Assets/BroncoHacks2025Photos/overview4.JPG";
+import Overview5 from "../Assets/BroncoHacks2025Photos/overview5.jpg";
+import Overview6 from "../Assets/BroncoHacks2025Photos/overview6.jpg";
+import judges from "../Assets/BroncoHacks2025Photos/judges.JPG";
+import Banner from "../Assets/BroncoHacks_Banner_ScreenWidth_2.png";
+import ClickableBanner from "../Assets/ClickableBanner.png";
+
+function Home() {
+  const [mouseHover, setMouseHover] = useState(false);
+
+  const slidesData = [
+      { url: judges },
+      { url: Overview1 },
+      { url: Overview2 },
+      { url: Overview3 },
+      { url: Overview4 },
+      { url: Overview5 },
+      { url: Overview6 },
+  ];
+
+  return (
+    <>
+      <div className="home-featured-container">
+        <div className="banner-container">
+          {mouseHover ? (
+            <img src={ClickableBanner} className="broncohacks_banner"></img>
+          ) : (
+            <img src={Banner} className="broncohacks_banner"></img>
+          )}
+
+          {/* <img src={Banner1} className="broncohacks_banner"></img> */}
+          {/* <a href="https://broncohacksportal.org/" class="sign-up-link" target="_blank" onMouseEnter={() => setMouseHover(true)} onMouseLeave={() => setMouseHover(false)} style={{ cursor: mouseHover ? `url(${bruh}), auto` : "pointer" }}> */}
+          <a
+            // href="https://www.broncohacksportal.org/"
+            href="https://forms.gle/2ZYGJNLVgud5apyX8"
+            className="sign-up-link"
+            target="_blank"
+            onMouseEnter={() => setMouseHover(true)}
+            onMouseLeave={() => setMouseHover(false)}
+          ></a>
+        </div>
+        {/* <img src={Banner2} className="broncohacks_banner"></img> */}
+
+        <a
+          className="register-button"
+          // href="https://www.broncohacksportal.org/"
+          href="https://forms.gle/2ZYGJNLVgud5apyX8"
+          target="_blank"
+        >
+          Register Now!
+        </a>
+
+        <Countdown
+          deadline="April, 25, 2026"
+          title="BroncoHacks 2026"
+        />
+
+        <div className="home-featured-row">
+          <div className="description-box">
+            <DescriptionBox />
+          </div>
+          <div className="containerStyles">
+            <Slideshow slides={slidesData} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
