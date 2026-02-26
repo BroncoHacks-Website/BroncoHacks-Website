5e847b7b5fc47bf735de9ee596776aa9413cd2ae
Mandy <mandyly13@gmail.com>
2026-02-25 17:12:13 -0800

    Created NavBar Component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import NavBar from "../Components/NavBar";
 
 function Home() {
 
   return (
     <> 
+      <NavBar />
       <div>
-        <h2>Hello team! Feel free to edit, refactor, or clean up anything as you see fit. If possible try and follow the file logic so it's uniform. You’re welcome to reuse or adapt code from previous years if it helps. Looking forward to seeing the BroncoHacks site come together!</h2>
-        <a target="_blank" href="https://broncohacks.org/">Current BroncoHacks</a>
+        {/* <h2>Hello team! Feel free to edit, refactor, or clean up anything as you see fit. If possible try and follow the file logic so it's uniform. You’re welcome to reuse or adapt code from previous years if it helps. Looking forward to seeing the BroncoHacks site come together!</h2> */}
+        {/* <a target="_blank" href="https://broncohacks.org/">Current BroncoHacks</a> */}
       </div>
     </>
   );
