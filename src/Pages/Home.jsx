30d03912a4ac79363cc9e8a10f5c693631934880
Caleb C <kaichun24@gmail.com>
2026-02-25 22:34:47 -0800

    add qa

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,8 +5,7 @@ function Home() {
   return (
     <> 
       <div>
-        <h2>Hello team! Feel free to edit, refactor, or clean up anything as you see fit. If possible try and follow the file logic so it's uniform. You’re welcome to reuse or adapt code from previous years if it helps. Looking forward to seeing the BroncoHacks site come together!</h2>
-        <a target="_blank" href="https://broncohacks.org/">Current BroncoHacks</a>
+
       </div>
     </>
   );
