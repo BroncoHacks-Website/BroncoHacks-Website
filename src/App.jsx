6f2647bf7faae901e76bf16be2c54243f5e767c5
Solares10 <solaresmichelle22@gmail.com>
2026-03-09 22:20:41 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+
 import Home from "./Pages/Home.jsx";
 import HallOfFame from "./Pages/HallOfFame.jsx";
+// import Team from "./Pages/Team.jsx";
+// import SponsorsPage from "./Components/SponsorsPage.jsx";
 
 function App() {
   return (
-    // <BrowserRouter basename="/BroncoHacks-Website/"></BrowserRouter>
-    <BrowserRouter className = "content" style = {{flex: 1}}>
+    <BrowserRouter>
+      {/* 
+        If deploying to GitHub Pages later, use:
+        <BrowserRouter basename="/BroncoHacks-Website">
+      */}
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/hallOfFame" element={<HallOfFame />}></Route>
+        <Route path="/hallOfFame" element={<HallOfFame />} />
+        {/* <Route path="/team" element={<Team />} /> */}
+        {/* <Route path="/sponsors" element={<SponsorsPage />} /> */}
       </Routes>
+
     </BrowserRouter>
   );
 }
+
 export default App;
