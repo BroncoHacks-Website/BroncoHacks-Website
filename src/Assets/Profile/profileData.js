868e3f59b0c8a696162a0950c6c6f1be5c07b49b
Caleb C <kaichun24@gmail.com>
2026-03-25 01:06:36 -0700

    add lots of people

--- src/Assets/Profile/profileData.js
+++ src/Assets/Profile/profileData.js
@@ -10,9 +10,29 @@ import KoenSo from "./Team/KoenSo.jpg";
 import MandyLy from "./Team/MandyLy.png";
 import GlorieleMendoza from "./Team/GlorieleMendoza.jpg";
 import JessicaPinto from "./Team/JessicaPinto.jpg";
-import Default from "./Team/Default.png";
+import Default from "../../BroncoHacks2025/2025Assets/Profile/team/user.png";
 import RachelLam from "./Team/RachelLam.jpg";
 import JaydenNguyen from "./Team/JaydenNguyen.jpeg";
+import BrianTran from "./Team/BrianTran.png";
+import CaseyVillalon from "./Team/CaseyVillalon.jpg";
+import SayumiAmarasinghe from "./Team/SayumiAmarasinghe.jpeg";
+import JustinNguyen from "../../BroncoHacks2025/2025Assets/Profile/team/JustinNguyen.jpg";
+import TonyTong from "../../BroncoHacks2025/2025Assets/Profile/team/TonyTong.jpeg";
+import MeganBee from "../../BroncoHacks2025/2025Assets/Profile/team/MeganBee.jpeg";
+import ErikaLedesma from "../../BroncoHacks2025/2025Assets/Profile/team/ErikaLedesma.jpg";
+import BradKim from "../../BroncoHacks2025/2025Assets/Profile/team/BradKim.jpeg";
+import StephaniePocci from "../../BroncoHacks2025/2025Assets/Profile/team/StephaniePocci.jpeg";
+import JohnnyFok from "../../BroncoHacks2025/2025Assets/Profile/team/JohnnyFok.jpeg";
+import KeerthiSreeram from "../../BroncoHacks2025/2025Assets/Profile/team/KeerthiSreeram.jpg";
+import ChrisJiChekLo from "../../BroncoHacks2025/2025Assets/Profile/team/ChrisJLo.png";
+import AmarGandhi from "../../BroncoHacks2025/2025Assets/Profile/team/AmarGandhi.jpg";
+import PhuTroung from "../../BroncoHacks2025/2025Assets/Profile/team/PhuTruong.png";
+import AlexEckert from "../../BroncoHacks2025/2025Assets/Profile/team/AlexEckert.jpg";
+import KaylaTang from "../../BroncoHacks2025/2025Assets/Profile/team/KaylaTang.jpg";
+import JonahMokhtarzadeh from "../../BroncoHacks2025/2025Assets/Profile/team/JonahMokhtarzadeh.jpg";
+import LeslyIbarra from "../../BroncoHacks2025/2025Assets/Profile/team/LeslyIbarra.jpg";
+import GiselleAvila from "../../BroncoHacks2025/2025Assets/Profile/team/GiselleAvila.png";
+import AmandaChang from "./Team/AmandaChang.webp";
 
 export const profileData = [
     {
@@ -25,7 +45,7 @@ export const profileData = [
         ],
         picture: MichaelWu,
         linkedin: "https://www.linkedin.com/in/michael-ml-wu/",
-        github: "https://github.com/mwu",
+        github: "https://github.com/michaelwuhu",
     },
     {
         id: 1,
@@ -177,8 +197,224 @@ export const profileData = [
         ],
         picture: Default,
         linkedin: "https://www.linkedin.com/in/joseline-ly/"
+    },
+    {
+        id: 16,
+        name: "Brian Tran",
+        role: [
+            "Finance"
+        ],
+        picture: BrianTran,
+        linkedin: "http://www.linkedin.com/in/brianphantran"
+    },
+    {
+        id: 17,
+        name: "Casey Villalon",
+        role: [
+            "Design"
+        ],
+        picture: CaseyVillalon,
+        linkedin: "https://linkedin.com/in/casey-villalon",
+        portfolio: "https://kay.design"
+    },
+    {
+        id: 18,
+        name: "Sayumi Amarasinghe",
+        role: [
+            "Website Developer"
+        ],
+        picture: SayumiAmarasinghe,
+        linkedin: "https://www.linkedin.com/in/sayumi-amarasinghe/",
+        github: "https://github.com/SayumiAmarasinghe"
+    },
+    /* START PAST COMMITTEES minus a few */
+    {
+        id: 19,
+        name: "Justin Nguyen",
+        role: [
+            "BroncoHacks Committee 2024-2025",
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: JustinNguyen,
+        linkedin: "https://www.linkedin.com/in/justin-mn/"
+    },
+    {
+        id: 20,
+        name: "UyenNghi Nguyen",
+        role: [
+            "BroncoHacks Committee 2024-2025",
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: Default,
+        linkedin: "https://www.linkedin.com/"
+    },
+    {
+        id: 21,
+        name: "Aidan Sanders",
+        role: [
+            "Primary Organizer",
+            "BroncoHacks Committee 2024-2025",
+            "BroncoHacks Committee 2023-2024",
+        ],
+        picture: Default,
+        linkedin: "https://www.linkedin.com/in/aidan-sanders-756548276/"
+    },
+    {
+        id: 22,
+        name: "Tony Tong",
+        role: [
+            "Primary Organizer",
+            "BroncoHacks Committee 2024-2025",
+        ],
+        picture: TonyTong,
+        linkedin: "https://www.linkedin.com/in/tony-tong-699631240/"
+    },
+    {
+        id: 23,
+        name: "Megan Bee",
+        role: [
+            "Primary Organizer",
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: MeganBee,
+        linkedin: "https://www.linkedin.com/in/megan-bee/"
+    },
+    {
+        id: 24,
+        name: "Erika Ledesma",
+        role: [
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: ErikaLedesma,
+        linkedin: "https://www.linkedin.com/in/erika-jasmine-ledesma/"
+    },
+    {
+        id: 25,
+        name: "Brad Kim",
+        role: [
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: BradKim,
+        linkedin: "https://www.linkedin.com/in/bradjkim/"
+    },
+    {
+        id: 26,
+        name: "Stephanie Pocci",
+        role: [
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: StephaniePocci,
+        linkedin: "https://www.linkedin.com/in/stephanie-pocci/"
+    },
+    {
+        id: 27,
+        name: "Johnny Fok",
+        role: [
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: JohnnyFok,
+        linkedin: "https://www.linkedin.com/in/johnnyfok/"
+    },
+    {
+        id: 28,
+        name: "Keerthi Sreeram",
+        role: [
+            "BroncoHacks Committee 2024-2025"
+        ],
+        picture: KeerthiSreeram,
+        linkedin: "https://www.linkedin.com/in/keerthi-sreeram/"
+    },
+    {
+        id: 29,
+        name: "Chris Ji-Chek Lo",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: ChrisJiChekLo,
+        linkedin: "https://www.linkedin.com/in/christopher-j-lo/"
+    },
+    {
+        id: 30,
+        name: "Amar Gandhi",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: AmarGandhi,
+        linkedin: "https://www.linkedin.com/in/acgandhi/"
+    },
+    {
+        id: 31,
+        name: "Phu Troung",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: PhuTroung,
+        linkedin: "https://www.linkedin.com/in/phuntruong/",
+    },
+    {
+        id: 32,
+        name: "Alex Eckert",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: AlexEckert,
+        linkedin: "https://www.linkedin.com/in/alexander-eckert-07b424257/"
+    },
+    {
+        id: 33,
+        name: "Kayla Tang",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: KaylaTang,
+        linkedin: "https://www.linkedin.com/in/kayla-tang/"
+    },
+    {
+        id: 34,
+        name: "Jonah Mokhtarzadeh",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: JonahMokhtarzadeh,
+        linkedin: "https://www.linkedin.com/in/jonah-mokhtarzadeh-97163b187/"
+    },
+    {
+        id: 35,
+        name: "Lesly Ibarra",
+        role: [
+            "BroncoHacks Committee 2023-2024",
+        ],
+        picture: LeslyIbarra,
+        linkedin: "https://www.linkedin.com/in/lesly-d-ibarra-720bb5273/"
+    },
+    {
+        id: 36,
+        name: "Giselle Avila",
+        role: [
+            "BroncoHacks Committee 2023-2024",
+        ],
+        picture: GiselleAvila,
+        linkedin: "https://www.linkedin.com/in/giselleavila/"
+    },
+    {
+        id: 37,
+        name: "Joshua Vejendla",
+        role: [
+            "BroncoHacks Committee 2023-2024"
+        ],
+        picture: Default,
+        linkedin: "https://www.linkedin.com"
+    },
+    /* END PAST COMMITTEES */
+    {
+        id: 38,
+        name: "Amanda Chang",
+        role: [
+            "Finance"
+        ],
+        picture: AmandaChang,
+        linkedin: "http://www.linkedin.com/in/amanda-changg"
     }
-  
 ];
 
 export default profileData;
