c2a3e24d559a743b3bd6111d7673ae46d4defbbe
Caleb C <kaichun24@gmail.com>
2026-04-08 01:42:48 -0700

    add cloud links to the pics so that them 5mb images are not as massive and the carousel doesnt start lagging cuz its like 50mb of pics

--- src/Assets/data/HOF2025.jsx
+++ src/Assets/data/HOF2025.jsx
@@ -1,39 +1,33 @@
-import Winner1 from "../winners/2025/Winner_1.JPG";
-import Winner2 from "../winners/2025/Winner_2.JPG";
-import Winner3 from "../winners/2025/Winner_3.JPG";
-import Winner4 from "../winners/2025/Winner_4.JPG";
-import Winner5 from "../winners/2025/Winner_5.JPG";
-
 const winners = [
     {
         placement: "1ST",
         title: "MediFind",
         description: "Medication management, price comparison, and safety app.",
-        img: Winner1
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Winner_1_jmnoh5",
     },
     {
         placement: "2ND",
         title: "QuishThis",
         description: "Protect yourself from malicious QR codes.",
-        img: Winner2,
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Winner_2_l0wvmv.jpg",
     },
     {
         placement: "3RD",
         title: "Carbon Closet",
         description: "Interactive thrifting with token rewards.",
-        img: Winner3
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Winner_3_vyfqdc.jpg",
     },
     {
         placement: "4TH",
         title: "Honey Hack",
         description: "N/A",
-        img: Winner4
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Winner_4_o32ebl.jpg",
     },
     {
         placement: "5TH",
         title: "TA AI",
         description: "AI-powered teaching assistant.",
-        img: Winner5
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Winner_5_rnusdo.jpg",
     }
   ];
 
--- src/Assets/data/HOF2025_cat.jsx
+++ src/Assets/data/HOF2025_cat.jsx
@@ -1,40 +1,33 @@
-import WebDev from "../winners/2025/Best_Web_Dev.JPG";
-import DSAI from "../winners/2025/BEST_DSAI.JPG";
-import GameDev from "../winners/2025/Best_Game_Dev.JPG";
-import UIUX from "../winners/2025/Best_UIUX.JPG";
-import Cyber from "../winners/2025/Best_Cyber_Sec.JPG";
-import TeamName from "../winners/2025/Best_Team_Name.JPG";
-
 const winners = [
     {
         placement: "WEB DEV",
         title: "Carbon Closet",
         description: "AI-powered teaching assistant...",
-        img: WebDev
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Best_Web_Dev_ohkii9.jpg"
     },
     {
         placement: "DS/AI",
         title: "MediFind",
         description: "Medication management, price comparison, and safety app.",
-        img: DSAI
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/BEST_DSAI_gfckri.jpg"
     },
     {
         placement: "GAME DEV",
         title: "QuishThis",
         description: "Protect yourself from malicious QR codes.",
-        img: GameDev
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Best_Game_Dev_abqxst.jpg"
     },
     {
         placement: "UI/UX",
         title: "Honey Hack",
         description: "N/A",
-        img: UIUX
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Best_UIUX_vnl8xd.jpg"
     },
     {
         placement: "CYBER",
         title: "TA AI",
         description: "AI-powered teaching assistant.",
-        img: Cyber
+        img: "https://res.cloudinary.com/dtknolvtz/image/upload/f_auto,q_auto,w_800/Best_Cyber_Sec_ngzg6t.jpg"
     }
   ];
 
